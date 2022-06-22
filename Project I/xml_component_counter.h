c1993837911cdf9eb089eb0729ae4a7606d700d4
matheusAparicio <matheusilva-m@outlook.com>
2022-06-21 22:51:38 -0300

    xmlComponentCounter more logic implemented

--- Project I/xml_component_counter.h
+++ Project I/xml_component_counter.h
@@ -40,6 +40,7 @@ void xmlComponentCounter(char fileName[100]) {
         bool canGetData = false;
         int width = 0;
         int height = 0;
+        int data[width][height];
 
         string line;
         ifstream in(fileName);
@@ -48,30 +49,37 @@ void xmlComponentCounter(char fileName[100]) {
             
             for (int i = 0; i < line.length(); i++) {
 
-                if (canGetData) {
-
-                }
-
                 // Define o tamanho da imagem.
                 if (line[i] == '<') {
                     if (getTag(line.substr(i)) == "<width>") {
                         width = getSize(line.substr(i));
+                        cout << "AAAAAA " << width << endl;
                     } else if ((getTag(line.substr(i)) == "<height>")) {
                         height = getSize(line.substr(i));
+                        cout << "AAAAAA " << height << endl;
                     }
                 }
-                
+
                 // Define quando é possível pegar a imagem.
                 if (line[i] == '<') {
                     if (getTag(line.substr(i)) == "<data>") {
                         canGetData = true;
-                        break;
+                        int data[width][height];
                     } else if ((getTag(line.substr(i)) == "</data>")) {
                         canGetData = false;
                     }
+                    break;
                 }
+
+                if (canGetData) {
+                    cout << line[i] << " ";
+                    //data[i][lineCount] = line[i];
+                }
+
             }
+                
             lineCount++;
+            cout << endl;
         }
 
     } else {
